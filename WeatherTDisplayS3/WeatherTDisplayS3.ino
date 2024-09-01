b1b67782b127a8fbc9c8e1352b0047095ff2abb0
john mclaughlin <john.n.mclaughlin@gmail.com>
2024-08-31 20:57:17 -0400

    Personalize to Somerville

--- WeatherTDisplayS3/WeatherTDisplayS3.ino
+++ WeatherTDisplayS3/WeatherTDisplayS3.ino
@@ -17,10 +17,10 @@ ESP32Time rtc(0);
 
 //#################### EDIT THIS  ###################
 //time zone  
-int zone = 2;
-String town = "Zagreb";
-String myAPI = "d0d0bf1bb46822e5dce67c95f4fd0800";
-String units = "metric";  //  metric, imperial
+int zone = -4;
+String town = "Somerville";
+String myAPI = "fb6f79759f657aaa0304e002c2ea08f3";
+String units = "imperial";  //  metric, imperial
 //#################### end of edits ###################
 
 
@@ -37,7 +37,7 @@ int counter=0;
 
 //................colors
 #define bck TFT_BLACK
-unsigned short grays[13];
+unsigned short yellows[13];
 
 // static strings of data showed on right side 
 char* PPlbl[] = { "HUM", "PRESS", "WIND" };
@@ -96,11 +96,11 @@ void setup() {
   setTime();
   getData();
 
-  // generate 13 levels of gray
-  int co = 210;
+  // generate 13 levels of yellow
+  int intensity = 255;
   for (int i = 0; i < 13; i++) {
-    grays[i] = tft.color565(co, co, co);
-    co = co - 20;
+    yellows[i] = tft.color565(intensity, intensity, 0);  // yellow shade
+    intensity = intensity - 20;  // decrease the intensity
   }
 }
 
@@ -149,62 +149,62 @@ void getData() {
 
 void draw() {
 
-  errSprite.fillSprite(grays[10]);
-  errSprite.setTextColor(grays[1], grays[10]);
+  errSprite.fillSprite(yellows[10]);
+  errSprite.setTextColor(yellows[1], yellows[10]);
   errSprite.drawString(Wmsg, ani, 4);
 
   sprite.fillSprite(TFT_BLACK);
-  sprite.drawLine(138, 10, 138, 164, grays[6]);
-  sprite.drawLine(100, 108, 134, 108, grays[6]);
+  sprite.drawLine(138, 10, 138, 164, yellows[6]);
+  sprite.drawLine(100, 108, 134, 108, yellows[6]);
   sprite.setTextDatum(0);
 
   //LEFTSIDE
   sprite.loadFont(midleFont);
-  sprite.setTextColor(grays[1], TFT_BLACK);
+  sprite.setTextColor(yellows[1], TFT_BLACK);
   sprite.drawString("WEATHER", 6, 10);
   sprite.unloadFont();
 
   sprite.loadFont(font18);
-  sprite.setTextColor(grays[7], TFT_BLACK);
+  sprite.setTextColor(yellows[7], TFT_BLACK);
   sprite.drawString("TOWN:", 6, 110);
-  sprite.setTextColor(grays[2], TFT_BLACK);
+  sprite.setTextColor(yellows[2], TFT_BLACK);
   if (units == "metric")
     sprite.drawString("C", 14, 50);
   if (units == "imperial")
     sprite.drawString("F", 14, 50);
 
  
-  sprite.setTextColor(grays[3], TFT_BLACK);
+  sprite.setTextColor(yellows[3], TFT_BLACK);
   sprite.drawString(town, 46, 110);
-  sprite.fillCircle(8, 52, 2, grays[2]);
+  sprite.fillCircle(8, 52, 2, yellows[2]);
   sprite.unloadFont();
 
   // draw wime without seconds
   sprite.loadFont(tinyFont);
-  sprite.setTextColor(grays[4], TFT_BLACK);
+  sprite.setTextColor(yellows[4], TFT_BLACK);
   sprite.drawString(rtc.getTime().substring(0, 5), 6, 132);
   sprite.unloadFont();
 
   // draw some static text
-  sprite.setTextColor(grays[5], TFT_BLACK);
+  sprite.setTextColor(yellows[5], TFT_BLACK);
   sprite.drawString("INTERNET", 86, 10);
   sprite.drawString("STATION", 86, 20);
-  sprite.setTextColor(grays[7], TFT_BLACK);
+  sprite.setTextColor(yellows[7], TFT_BLACK);
   sprite.drawString("SECONDS", 92, 157);
 
   // draw temperature
   sprite.setTextDatum(4);
   sprite.loadFont(bigFont);
-  sprite.setTextColor(grays[0], TFT_BLACK);
+  sprite.setTextColor(yellows[0], TFT_BLACK);
   sprite.drawFloat(temperature, 1, 69, 80);
   sprite.unloadFont();
 
 
   //draw sec rectangle
-  sprite.fillRoundRect(90, 132, 42, 22, 2, grays[2]);
+  sprite.fillRoundRect(90, 132, 42, 22, 2, yellows[2]);
   //draw seconds
   sprite.loadFont(font18);
-  sprite.setTextColor(TFT_BLACK, grays[2]);
+  sprite.setTextColor(TFT_BLACK, yellows[2]);
   sprite.drawString(rtc.getTime().substring(6, 8), 111, 144);
   sprite.unloadFont();
 
@@ -212,16 +212,16 @@ void draw() {
   sprite.setTextDatum(0);
   //RIGHT SIDE
   sprite.loadFont(font18);
-  sprite.setTextColor(grays[1], TFT_BLACK);
+  sprite.setTextColor(yellows[1], TFT_BLACK);
   sprite.drawString("LAST 12 HOUR", 144, 10);
   sprite.unloadFont();
 
-  sprite.fillRect(144, 28, 84, 2, grays[10]);
+  sprite.fillRect(144, 28, 84, 2, yellows[10]);
 
-  sprite.setTextColor(grays[3], TFT_BLACK);
+  sprite.setTextColor(yellows[3], TFT_BLACK);
   sprite.drawString("MIN:" + String(minT), 254, 10);
   sprite.drawString("MAX:" + String(maxT), 254, 20);
-  sprite.fillSmoothRoundRect(144, 34, 174, 60, 3, grays[10], bck);
+  sprite.fillSmoothRoundRect(144, 34, 174, 60, 3, yellows[10], bck);
   sprite.drawLine(170, 39, 170, 88, TFT_WHITE);
   sprite.drawLine(170, 88, 314, 88, TFT_WHITE);
 
@@ -229,33 +229,33 @@ void draw() {
 
   for (int j = 0; j < 24; j++)
     for (int i = 0; i < PPgraph[j]; i++)
-      sprite.fillRect(173 + (j * 6), 83 - (i * 4), 4, 3, grays[2]);
+      sprite.fillRect(173 + (j * 6), 83 - (i * 4), 4, 3, yellows[2]);
 
-  sprite.setTextColor(grays[2], grays[10]);
+  sprite.setTextColor(yellows[2], yellows[10]);
   sprite.drawString("MAX", 158, 42);
   sprite.drawString("MIN", 158, 86);
 
   sprite.loadFont(font18);
-  sprite.setTextColor(grays[7], grays[10]);
+  sprite.setTextColor(yellows[7], yellows[10]);
   sprite.drawString("T", 158, 58);
   sprite.unloadFont();
 
 
   for (int i = 0; i < 3; i++) {
-    sprite.fillSmoothRoundRect(144 + (i * 60), 100, 54, 32, 3, grays[9], bck);
-    sprite.setTextColor(grays[3], grays[9]);
+    sprite.fillSmoothRoundRect(144 + (i * 60), 100, 54, 32, 3, yellows[9], bck);
+    sprite.setTextColor(yellows[3], yellows[9]);
     sprite.drawString(PPlbl[i], 144 + (i * 60) + 27, 107);
-    sprite.setTextColor(grays[2], grays[9]);
+    sprite.setTextColor(yellows[2], yellows[9]);
     sprite.loadFont(font18);
     sprite.drawString(String((int)wData[i])+PPlblU[i], 144 + (i * 60) + 27, 124);
     sprite.unloadFont();
 
-    sprite.fillSmoothRoundRect(144, 148, 174, 16, 2, grays[10], bck);
+    sprite.fillSmoothRoundRect(144, 148, 174, 16, 2, yellows[10], bck);
     errSprite.pushToSprite(&sprite, 148, 150);
   }
-  sprite.setTextColor(grays[4], bck);
+  sprite.setTextColor(yellows[4], bck);
   sprite.drawString("CURRENT WEATHER", 190, 141);
-  sprite.setTextColor(grays[9], bck);
+  sprite.setTextColor(yellows[9], bck);
    sprite.drawString(String(counter), 310, 141);
 
   sprite.pushSprite(0, 0);
